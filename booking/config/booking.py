d0805c89b8647445e2276a403bd99a2c274b9941
britlog <info@britlog.com>
2018-08-16 21:14:26 +0200

    Manage layout categories

---
+++ booking/config/booking.py
@@ -0,0 +1,49 @@
+from __future__ import unicode_literals
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"label": _("Bookings"),
+			"icon": "fa fa-star",
+			"items": [
+				{
+					"type": "doctype",
+					"name": "Booking Slot",
+					"description": _("Booking Slots."),
+				},
+				{
+					"type": "doctype",
+					"name": "Booking",
+					"description": _("Web Bookings."),
+				},
+			]
+		},
+		{
+			"label": _("Reports"),
+			"icon": "fa fa-list",
+			"items": [
+				{
+					"type": "report",
+					"is_query_report": True,
+					"name": "Attendance Sheet"
+				},
+			]
+		},
+		{
+			"label": _("Setup"),
+			"icon": "fa fa-cog",
+			"items": [
+				{
+					"type": "doctype",
+					"name": "Booking Settings",
+					"description": _("Default settings for booking transactions.")
+				},
+				{
+					"type": "doctype",
+					"name": "Booking Type",
+					"description": _("Booking Type."),
+				},
+			]
+		},
+	]
