00f6ae15fcb4b066e489e04ce15711806ecd1ca0
britlog <info@britlog.com>
2019-08-21 22:59:18 +0200

    Change doc event to update time slot display if time slot is updated

--- booking/booking/doctype/booking_slot/booking_slot.py
+++ booking/booking/doctype/booking_slot/booking_slot.py
@@ -15,10 +15,8 @@ class BookingSlot(Document):
     #     location = " " + self.location if self.location else ""
     #     self.name = frappe.utils.format_datetime(self.time_slot,"EEEE dd/MM/yyyy HH:mm").capitalize() + location
 
-    def before_insert(self):
-        self.time_slot_display = frappe.utils.format_datetime(self.time_slot,"EEEE dd/MM/yyyy HH:mm").capitalize()
-
     def validate(self):
+        self.time_slot_display = frappe.utils.format_datetime(self.time_slot, "EEEE dd/MM/yyyy HH:mm").capitalize()
         trigger_notification(self)
 
     def on_update(self):
