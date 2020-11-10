e1cc2bd87c1afbc5958ee3973bfa6298dd9274e5
britlog <info@britlog.com>
2020-11-10 14:58:13 +0100

    fix email id fetch if email_id is empty on customer master

--- booking/patches/set_class_coefficient.py
+++ booking/patches/set_class_coefficient.py
@@ -2,7 +2,9 @@ from __future__ import unicode_literals
 import frappe
 
 def execute():
-
+	frappe.reload_doctype("Booking Subscriber")
 	frappe.db.sql(""" UPDATE `tabBooking Subscriber` set class_coefficient = 1 """)
+
+	frappe.reload_doctype("Booking Class")
 	frappe.db.sql(""" UPDATE `tabBooking Class` set class_coefficient = 1 """)
 
